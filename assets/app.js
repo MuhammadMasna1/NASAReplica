acce312b419168a07d0e6ad7afe47484fc70b927
MuhammadMasna1 <157140756+MuhammadMasna1@users.noreply.github.com>
2024-02-02 16:32:30 +0500

    files relocated

---
+++ assets/app.js
@@ -0,0 +1,33 @@
+//!================= header side Bar =================
+let header = document.querySelector(".header");
+let searchBar = document.querySelector(".sideBar-btn");
+let menuBar = document.querySelector(".menu-bar");
+let toggleBtn = document.querySelector(".toggle-btn");
+console.log(menuBar);
+searchBar.addEventListener("click", () => {
+  header.classList.toggle("show-sideBar");
+});
+
+toggleBtn.addEventListener("click", () => {
+  header.classList.toggle("show-sideBar");
+});
+
+//!================= header Sub Menu =================
+let linkWrapper = document.querySelectorAll(".link-wrapper");
+let subMenu = document.querySelector(".subMenu");
+let wrapperOne = document.querySelector(".wrapper-one");
+let wrapperTwo = document.querySelector(".wrapper-two");
+
+linkWrapper.forEach((val) => {
+  val.addEventListener("click", () => {
+    val.classList.toggle("show");
+  });
+});
+
+wrapperOne.addEventListener("click", () => {
+  wrapperTwo.classList.remove("show");
+});
+
+wrapperTwo.addEventListener("click", () => {
+  wrapperOne.classList.remove("show");
+});
